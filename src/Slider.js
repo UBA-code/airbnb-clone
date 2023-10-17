14f8fb55869bb547a724638907b9bb9be17d57a5
Yassine Bel Hachmi <ybel-hac@e2r10p12.1337.ma>
2023-10-17 13:27:04 +0100

    update

--- src/Slider.js
+++ src/Slider.js
@@ -2,32 +2,25 @@ import './Slider.css'
 import { useEffect, useState } from 'react';
 
 export default function Slider({ imgs }) {
-	console.log("rendred");
-	let [img, setImg] = useState(imgs[0]);
-	let [name, setName] = useState('');
+	let [index, setIndex] = useState(0);
+	let [name, setName] = useState('show');
+
 	useEffect(_ => {
-		console.log('use effect');
-		// setTimeout(() => {
-		// 	// let prom = new Promise(resolve => {
-		// 	// 	setTimeout(() => {
-		// 	// 		console.log("hide");
-		// 	// 		setName('hide');
-		// 	// 		resolve();
-		// 	// 	}, 500)
-		// 	// })
-		// 	// 	.then(res => {
-		// 			setImg(imgs[Math.floor(Math.random() * imgs.length)]);
-		// 		// 	return new Promise(res => setTimeout(res, 4000));
-		// 		// })
-		// 		// .then(res => {
-		// 		// 	console.log("hide");
-		// 		// 	setName('show');
-		// 		// })
-		// }, 5000);
-	}, [img])
+		setTimeout(() => {
+			let prom = new Promise(resolve => {
+				setName('hide');
+				setTimeout(() => {
+					resolve();
+				}, 500)
+			})
+				.then(res => setIndex((i) => i + 1 < imgs.length ? i + 1 : 0))
+				.then(res => setName('show'))
+		}, 5000);
+	}, [index])
+
 	return (
 		<div className="slider-container">
-			<img className={`gallery-item ${name}`} src={img} alt="slider-item" />
+			<img className={`gallery-item ${name}`} src={imgs[index]} alt="slider-item" />
 		</div>
 	)
 }
